445e75a227a67b6d96f86ad97271454cdb40be90
yashbarsker031 <79683529+yashbarsker031@users.noreply.github.com>
2021-03-10 21:40:46 +0530

    create frame

---
+++ Demo1.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+class FDemo extends Frame
+{
+	FDemo()
+	{
+	}
+}
+class Demo1
+{
+	public static void main(String ar[])
+	{
+		FDemo f=new FDemo();
+		f.setVisible(true);
+		f.setSize(500,300);
+		f.setLocation(200,100);
+		
+	}
+}
